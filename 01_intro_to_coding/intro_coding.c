d859c49276b0e102f9e17431793feb3a44727727
iriana2006 <airizarry0777@floridapoly.edu>
2025-08-22 14:36:09 +0000

    Created the frist program

---
+++ 01_intro_to_coding/intro_coding.c
@@ -0,0 +1,20 @@
+/* 
+yo 8/20/25
+Anastasia Irizarry
+C program
+*/
+
+#include <stdio.h> //Preprocessor directive
+
+int main(void) {
+    
+    char user_name;
+
+    printf ("Hello World!"\n);
+        printf ("AHHHHHHHHH! what's your name?  ");
+            scanf ("%s", &user_name);
+
+printf ("Hello, %s", &user_name);
+
+return 0;
+}
